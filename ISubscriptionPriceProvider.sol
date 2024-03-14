82e8014318cd45e6f8b8f28d500ad120dc20f5ce
Bertrand Juglas <b@juglas.name>
2024-03-14 17:29:31 +0100

    publish all ABI

--- ISubscriptionPriceProvider.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.0;
-
-interface ISubscriptionPriceProvider {
-    /**
-     * Return the number of Arthera coins (in wei) for the provided USD value (in cents)
-     */
-    function centsToArthera(uint256 usdValue) external view returns (uint256);
-}
